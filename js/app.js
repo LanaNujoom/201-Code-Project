99bae22701e17a2841d1e63df883365678018f03
LanaNujoom <lanaalinujoom3@gmail.com>
2020-12-28 21:55:34 +0200

    Add Guess Game

---
+++ js/app.js
@@ -0,0 +1,95 @@
+'use strict';
+
+
+var userName=prompt('What is You Name ??')
+// console.log(userName);
+alert ('Hello '+ userName);
+
+var myName=prompt('Do You Think My Name is Lana ! - please answer with yes/no or y/n');
+// console.log(myName.toLowerCase());
+switch(myName.toLowerCase()){
+
+    case 'yes':
+    case 'y':
+        alert ('That is True My name is Lana');
+        break ;
+    case 'no':
+    case 'n':
+        alert(' False, My name is Lana !');
+        break;
+
+        
+
+}
+
+var myfavoriteColor=prompt('Do You Think I Like Red ! - please answer with yes/no or y/n');
+// console.log(myfavoriteColor.toLowerCase());
+switch(myfavoriteColor.toLowerCase()){
+
+    case 'yes':
+    case 'y':
+        alert ('That is True I Like Red');
+        break ;
+    case 'no':
+    case 'n':
+        alert(' False, My Favorite Color is Red !');
+        break;
+
+        
+
+}
+
+var myfavoriteprogrammingLanguage=prompt('Do You Think My Favorite Programming Language is Javascripts ! - please answer with yes/no or y/n');
+// console.log(myfavoriteprogrammingLanguage.toLowerCase());
+switch(myfavoriteprogrammingLanguage.toLowerCase()){
+
+    case 'yes':
+    case 'y':
+        alert ('That is True My Favorite Programming Language is Javascripts');
+        break ;
+    case 'no':
+    case 'n':
+        alert(' Not Ture, My Favorite Programming Language is Javascripts !');
+        break;
+
+        
+
+}
+
+var myfavoritehobby=prompt('Do You Think My Favorite Sport is Football ! - please answer with yes/no or y/n');
+// console.log(myfavoritehobby.toLowerCase());
+switch(myfavoritehobby.toLowerCase()){
+
+    case 'yes':
+    case 'y':
+        alert ('That is False :( My Favorite Sport is Basketball');
+        break ;
+    case 'no':
+    case 'n':
+        alert('Fantastic :) My Favorite Sport is Basketball!');
+        break;
+
+        
+
+}
+
+var myfavoritefood=prompt('Guess if I love Fettuccini or Not ! - please answer with yes/no or y/n');
+// console.log(myfavoritefood.toLowerCase());
+switch(myfavoritefood.toLowerCase()){
+
+    case 'yes':
+    case 'y':
+        alert ('Perefect I love Fettucini');
+        break ;
+    case 'no':
+    case 'n':
+        alert('But My Favorite Food is Fettuccini !');
+        break;
+
+        
+
+}
+
+
+
+alert ('Welcome to my site '+ userName);

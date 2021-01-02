3e263d8b3fefecce54f2a63083c710066d07bb34
LanaNujoom <lanaalinujoom3@gmail.com>
2021-01-02 23:53:32 +0200

    add css & functions

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
-# 201-Code-Project
+# 201-Code-Project Lab 03
+
+- made 5 questions to the user 'guessing game' with using conditions ( switch )
+
+
+
+# 201-Code-Project Lab 03
 
 # What Features Did I Add ??
+
+- Add 6th and 7th questions with loops
 The Website will have two loops one is for asking the user to input a color that the owner likes & the other is for guessing owner's age.
 
+-use of array
+
 ***There is a counter that counts how many correct answers the user had and the score will appear at the end of questions.***
+
+# 201-Code-Project Lab 04
+
+- made a new branch
+- add functions 
+
+
+*Driver : Yazan Eyad*
+
+*Navigator: Lana Nujoom*
+
+# 201-Code-Project Lab 05b
+
+CSS:
+
+- Add CSS and made the website prettier
+
+JavaScript's :
+
+-Remove all comments 
+
+- Remove console log comments
+
+- rename l variable to counter
+
+- make the 7 th questions as functions
+
+HTML:
+
+-add another image 
+
+
+
+
+
+
+
--- js/app.js
+++ js/app.js
@@ -1,163 +1,213 @@
 'use strict';
 
-var l = 0
-var userName=prompt('What is You Name ??')
-// console.log(userName);
-alert ('Hello '+ userName);
+var counter = 0
+var userName = prompt('What is You Name ??')
 
-var myName=prompt('Do You Think My Name is Lana ! - please answer with yes/no or y/n');
-// console.log(myName.toLowerCase());
-switch(myName.toLowerCase()){
+alert('Hello ' + userName);
 
-    case 'yes':
-    case 'y':
-        alert ('That is True My name is Lana');
-        l++
-        break ;
-    case 'no':
-    case 'n':
-        alert(' False, My name is Lana !');
-        break;
+function firstQuestion() {
+    var myName = prompt('Do You Think My Name is Lana ! - please answer with yes/no or y/n');
 
-        
+    switch (myName.toLowerCase()) {
 
-}
-
-var myfavoriteColor=prompt('Do You Think I Like Red ! - please answer with yes/no or y/n');
-// console.log(myfavoriteColor.toLowerCase());
-switch(myfavoriteColor.toLowerCase()){
-
-    case 'yes':
-    case 'y':
-        alert ('That is True I Like Red');
-        l++
-        break ;
-    case 'no':
-    case 'n':
-        alert(' False, My Favorite Color is Red !');
-        break;
-
-        
-
-}
-
-var myfavoriteprogrammingLanguage=prompt('Do You Think My Favorite Programming Language is Javascripts ! - please answer with yes/no or y/n');
-// console.log(myfavoriteprogrammingLanguage.toLowerCase());
-switch(myfavoriteprogrammingLanguage.toLowerCase()){
-
-    case 'yes':
-    case 'y':
-        alert ('That is True My Favorite Programming Language is Javascripts');
-        l++
-        break ;
-    case 'no':
-    case 'n':
-        alert(' Not Ture, My Favorite Programming Language is Javascripts !');
-        break;
-
-        
-
-}
-
-var myfavoritehobby=prompt('Do You Think My Favorite Sport is Football ! - please answer with yes/no or y/n');
-// console.log(myfavoritehobby.toLowerCase());
-switch(myfavoritehobby.toLowerCase()){
-
-    case 'yes':
-    case 'y':
-        alert ('That is False :( My Favorite Sport is Basketball');
-        break ;
-    case 'no':
-    case 'n':
-        alert('Fantastic :) My Favorite Sport is Basketball!');
-        l++
-        break;
-
-        
-
-}
-
-var myfavoritefood=prompt('Guess if I love Fettuccini or Not ! - please answer with yes/no or y/n');
-// console.log(myfavoritefood.toLowerCase());
-switch(myfavoritefood.toLowerCase()){
-
-    case 'yes':
-    case 'y':
-        alert ('Perefect I love Fettucini');
-        l++
-        break ;
-    case 'no':
-    case 'n':
-        alert('But My Favorite Food is Fettuccini !');
-        break;
-
-        
-
-}
+        case 'yes':
+        case 'y':
+            alert('That is True My name is Lana');
+            counter++
+            break;
+        case 'no':
+        case 'n':
+            alert(' False, My name is Lana !');
+            break;
 
 
 
-
-
-for( var i=0; i<4;  i++){
-
-    var myAge=prompt(' Guess How old am I (20-30)! ');
-     var num1 = parseInt(myAge);
-     console.log(num1);
-
-
-    if (num1 == 25) {
-
-        alert('That is Great , I am 25 years old');
-        l++
-        break;
-
-        } else if(num1 > 25){
-
-        alert('Too High , I am younger ');
-        
-    }
-          else {
-
-        alert(' Too Low, I am older' );
-       
     }
 
-
 }
 
+firstQuestion();
+
+function secondQuestion() {
+    var myfavoriteColor = prompt('Do You Think I Like Red ! - please answer with yes/no or y/n');
+
+    switch (myfavoriteColor.toLowerCase()) {
+
+        case 'yes':
+        case 'y':
+            alert('That is True I Like Red');
+            counter++
+            break;
+        case 'no':
+        case 'n':
+            alert(' False, My Favorite Color is Red !');
+            break;
 
 
-alert('I am 25 Years Old');
-
-var colorArray= ['red','yellow','blue','pink','green'];
-
-
-
-
-for( var x=0; x<6; x++){
-
-    var colorName= prompt( 'Can you guess one of the colors I like !').toLowerCase();
-
-    if  (colorName === colorArray[0]|| colorName === colorArray[1]||colorName === colorArray[2] || colorName === colorArray[3]|| colorName === colorArray[4] ) {
-        alert (' That is true');
-        l++
-        break;
-    } else {
-        alert ( 'Keep Trying Please');
 
     }
 
 
 
+}
+
+secondQuestion();
+
+function thirdQuestion() {
+
+    var myfavoriteprogrammingLanguage = prompt('Do You Think My Favorite Programming Language is Javascripts ! - please answer with yes/no or y/n');
+
+    switch (myfavoriteprogrammingLanguage.toLowerCase()) {
+
+        case 'yes':
+        case 'y':
+            alert('That is True My Favorite Programming Language is Javascripts');
+            counter++
+            break;
+        case 'no':
+        case 'n':
+            alert(' Not Ture, My Favorite Programming Language is Javascripts !');
+            break;
+
+
+
+    }
 
 }
-console.log(colorName.toLowerCase());
 
-alert('I like red, yellow,blue,pink and green :D ');
+thirdQuestion();
+
+function fourthQuestion() {
+
+    var myfavoritehobby = prompt('Do You Think My Favorite Sport is Football ! - please answer with yes/no or y/n');
+
+    switch (myfavoritehobby.toLowerCase()) {
+
+        case 'yes':
+        case 'y':
+            alert('That is False :( My Favorite Sport is Basketball');
+            break;
+        case 'no':
+        case 'n':
+            alert('Fantastic :) My Favorite Sport is Basketball!');
+            counter++
+            break;
 
 
-alert(l + ' Out of 7 is Your Score');       
 
-alert ('Welcome to my site '+ userName);
+    }
+
+}
+
+ fourthQuestion();
+
+
+
+function fifthQuestion() {
+
+
+    var myfavoritefood = prompt('Guess if I love Fettuccini or Not ! - please answer with yes/no or y/n');
+
+    switch (myfavoritefood.toLowerCase()) {
+
+        case 'yes':
+        case 'y':
+            alert('Perefect I love Fettucini');
+            counter++
+            break;
+        case 'no':
+        case 'n':
+            alert('But My Favorite Food is Fettuccini !');
+            break;
+
+
+
+    }
+
+}
+
+fifthQuestion();
+
+function sixthQuestion() {
+
+    for (var i = 0; i < 4; i++) {
+
+        var myAge = prompt(' Guess How old am I (20-30)! ');
+        var num1 = parseInt(myAge);
+
+
+
+        if (num1 === 25) {
+
+            alert('That is Great , I am 25 years old');
+            counter++
+            break;
+
+        } else if (num1 > 25) {
+
+            alert('Too High , I am younger ');
+
+        }
+        else {
+
+            alert(' Too Low, I am older');
+
+        }
+
+
+    }
+
+
+
+    alert('I am 25 Years Old');
+
+
+
+}
+
+sixthQuestion();
+
+
+
+
+function seventhQuestion() {
+
+    var colorArray = ['red', 'yellow', 'blue', 'pink', 'green'];
+
+
+
+
+    for (var x = 0; x < 6; x++) {
+
+        var colorName = prompt('Can you guess one of the colors I like !').toLowerCase();
+
+        if (colorName === colorArray[0] || colorName === colorArray[1] || colorName === colorArray[2] || colorName === colorArray[3] || colorName === colorArray[4]) {
+            alert(' That is true');
+            counter++
+            break;
+        } else {
+            alert('Keep Trying Please');
+
+        }
+
+
+
+
+    }
+
+
+    alert('I like red, yellow,blue,pink and green :D ');
+
+
+}
+
+seventhQuestion();
+
+
+
+
+
+alert(counter + ' Out of 7 is Your Score');
+
+alert('Welcome to my site ' + userName);
 
